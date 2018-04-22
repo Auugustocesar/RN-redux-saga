2a0eebc79f8bc4da828b501b3109aa9761f9f6e3
Augusto <augusto.gonzaga2010@gmail.com>
2018-04-22 20:41:55 -0300

    first commit

---
+++ src/pages/main/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import {
+  View,
+  StatusBar,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+
+import styles from './styles';
+
+const Main = () => (
+  <SafeAreaView style={styles.container}>
+    <StatusBar barStyle="light-content" />
+
+    <View style={styles.content}>
+      <Text style={styles.title}>GitMark</Text>
+      <Text style={styles.description}>
+        Comece adicionando alguns repositórios aos seus favoritos
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="usuario/repositório"
+          underlineColorAndroid="transparent"
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {}}
+          activeOpacity={0.6}
+        >
+          <Text style={styles.buttonText}>Adicionar aos favoritos</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+
+    <View style={styles.footer}>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style={styles.footerLink}>Meus favoritos (3)</Text>
+      </TouchableOpacity>
+    </View>
+  </SafeAreaView>
+);
+
+Main.navigationOptions = {
+  header: null,
+};
+
+export default Main;
